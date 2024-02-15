77d372c0f35aebe9b84756551fdc3997bd2f7e46
helloworld1317 <87151984+helloworld1317@users.noreply.github.com>
2024-02-15 14:06:55 -0800

    Fixing coin collection and checkpoint

---
+++ Assets/Scripts/Gameplay/GameMaster.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Platformer.Gameplay
+{
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster instance;
+    public Vector2 lastCheckPointPos;
+    void Awake(){
+        if (instance==null){
+            instance=this;
+            DontDestroyOnLoad(instance);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
---
+++ Assets/Scripts/Gameplay/PlayerPos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Platformer.Gameplay{
+
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm=GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position=gm.lastCheckPointPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
+}
