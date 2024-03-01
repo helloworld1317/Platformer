6dac742028b92e573591836ff79b96bcd228c664
helloworld1317 <87151984+helloworld1317@users.noreply.github.com>
2024-02-29 21:46:39 -0800

    TSA build

---
+++ Assets/Scripts/Mechanics/Dialogue.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public void NextSentence(){
+        if (index<sentences.Length-1){
+            index++;
+            textDisplay.text=" ";
+            StartCoroutine(Type());
+        }else{
+            textDisplay.text=" ";
+        }
+    }
+
+    IEnumerator Type(){
+        foreach(char letter in sentences[index].ToCharArray()){
+            textDisplay.text+=letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
